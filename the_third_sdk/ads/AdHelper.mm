51a5521e771a9b82f7ae9104271764f2b3ba0ea8
tanjie <tanjie0037@gmail.com>
2015-06-11 14:19:24 +0800

    update share sdk

--- the_third_sdk/ads/AdHelper.mm
+++ the_third_sdk/ads/AdHelper.mm
@@ -13,7 +13,7 @@
 #import <Foundation/Foundation.h>
 #import "AppController.h"
 #import "NativeXSDK.h"
-#import "SponsorPaySDK.h"
+//#import "SponsorPaySDK.h"
 #import "Supersonic/Supersonic.h"
 #include "SDKCommon.h"
 
@@ -153,9 +153,9 @@ static MyNaviteXDeleagte *_myNaviteXDeleagte = [[MyNaviteXDeleagte alloc] init];
 void AdHelper::initAd(AdType type, const std::string &uId, const std::string &appkey, const std::string &token) {
     switch (type) {
         case AdFyber:
-            [SponsorPaySDK startForAppId:[NSString stringWithUTF8String:appkey.c_str()]
-                                  userId:[NSString stringWithUTF8String:uId.c_str()]
-                           securityToken:[NSString stringWithUTF8String:token.c_str()]];
+//            [SponsorPaySDK startForAppId:[NSString stringWithUTF8String:appkey.c_str()]
+//                                  userId:[NSString stringWithUTF8String:uId.c_str()]
+//                           securityToken:[NSString stringWithUTF8String:token.c_str()]];
             break;
             
         case AdSupersonic:
@@ -178,11 +178,11 @@ void AdHelper::initAd(AdType type, const std::string &uId, const std::string &ap
 void AdHelper::callOfferwall(AdHelper::AdType type) {
     switch (type) {
         case AdFyber:
-            [SponsorPaySDK showOfferWallWithParentViewController:(UIViewController*)[AppController getRootView] completion: ^(int status) {
-                if (SPONSORPAY_ERR_NETWORK == status) {
-                    CCLOG("call offerwall failed.[%s, %s]", "Fyber", "SPONSORPAY_ERR_NETWORK");
-                }
-            }];
+//            [SponsorPaySDK showOfferWallWithParentViewController:(UIViewController*)[AppController getRootView] completion: ^(int status) {
+//                if (SPONSORPAY_ERR_NETWORK == status) {
+//                    CCLOG("call offerwall failed.[%s, %s]", "Fyber", "SPONSORPAY_ERR_NETWORK");
+//                }
+//            }];
             break;
             
         case AdSupersonic:
